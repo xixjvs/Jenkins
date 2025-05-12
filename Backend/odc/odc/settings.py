7c764a8fd96a950d780300a0b76b3400cdeb2b46
xixjvs <156476760+xixjvs@users.noreply.github.com>
2025-05-12 09:41:14 +0000

    Update settings.py

--- Backend/odc/odc/settings.py
+++ Backend/odc/odc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x*u4xobocld2xgun42d2)uh0p3y15y-$e!q3f$3-v2b#mfj+gh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = *
 
 
 # Application definition
