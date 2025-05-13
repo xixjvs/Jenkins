370569362faa92a91e8b8f74bf4ab05ae4d7316f
xixjvs <156476760+xixjvs@users.noreply.github.com>
2025-05-13 23:53:42 +0000

    Update api.jsx

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: " http://192.168.49.2:32379",
+    baseURL: " http://192.168.49.2:32379/api/",
 });
 
 export const UseAxios = () => {
